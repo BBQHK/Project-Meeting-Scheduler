65a221fa95bbcde91fbea12183b22df7e44d539b
ChingYuJianKenny <100947520+ChingYuJianKenny@users.noreply.github.com>
2022-03-27 15:47:27 +0800

    Update project_meeting_request.c

--- project_meeting_request.c
+++ project_meeting_request.c
@@ -38,6 +38,53 @@ void SingleInput(){
   }
 }
 
-// void BatchInput(){
-//
-// }
+void BatchInput(){
+	char dir[50];
+	char cmd[100];
+	
+	printf(" ~~ Project Meeting Request Single Input ~~\n");
+	while(1){
+		printf("Enter file name> ");
+		scanf("%s", &cmd);
+		if(!strcmp(cmd, "0")){
+			displayMenu();
+			chooseMenuOption();
+			break;
+		}
+		FILE * fp;
+		char * line = NULL;
+		size_t len = 0;
+		ssize_t read;
+		stpcpy(dir,"data/");
+		strcat(dir,cmd);
+		fp = fopen(dir, "r");
+		
+		if (fp == NULL)
+			exit(EXIT_FAILURE);
+		while ((read = getline(&line, &len, fp)) != -1) {
+			printf("Request: %s",line);
+			char *parts;
+			int i = 1;
+			parts = strtok(line, " ");
+			//printf("%d:%s\n",i,parts);
+			strcpy(bookingLists[bookingIndex].teamName, parts);
+			i++;
+			while((parts = strtok(NULL, " "))){
+				//printf("%d:%s\n",i,parts);
+				if(i == 2){
+					strcpy(bookingLists[bookingIndex].date, parts);
+				}else if(i == 3){
+					char temp[3];
+					sprintf(temp, "%c%c", parts[0], parts[1]);
+					bookingLists[bookingIndex].hour = atoi(temp);
+				}else if(i == 4){
+					bookingLists[bookingIndex].duration = atoi(parts);
+				}
+				i++;
+			}
+			bookingIndex++;
+		}
+		fclose(fp);
+		printf("Request has been received.\n");
+	}
+}
