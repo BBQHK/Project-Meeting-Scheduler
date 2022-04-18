09f67e4d200a38690fbc214d112d5b3f29bbf859
ChingYuJianKenny <100947520+ChingYuJianKenny@users.noreply.github.com>
2022-04-18 11:01:09 +0800

    Update global.h

--- global.h
+++ global.h
@@ -1,23 +1,25 @@
 struct Team
 {
-   char name[7];
-   char project_name[10];
-   char members[4][20];
-   int meetingRecord[5];
-   //0:Number of meetings held , 1:First member's meeting attendance...
+	char name[7];
+	char project_name[10];
+	char members[4][20];
+	int meetingRecord[5];
+	//0:Number of meetings held , 1:First member's meeting attendance...
+	int ReqRec;
+	int ReqAcc_FCFS;
+	int ReqAcc_SJF;
 };
 
 struct Booking
 {
-  char teamName[7];
-  char date[11];
-  int hour;
-  int duration;
-  int isAttended[5];//only store 0/1
-  //0:Has the booking been executed? , 1:is first member Attended ...
+	char teamName[7];
+	char date[11];
+	int hour;
+	int duration;
+	int isAttended[5];//only store 0/1
+	//0:Has the booking been executed? , 1:is first member Attended ...
+
 };
-
-
 
 struct Team teamLists[26];
 int teamsIndex = 0;
@@ -25,8 +27,17 @@ int teamsIndex = 0;
 struct Booking bookingLists[170];
 int bookingIndex = 0;
 
-struct Booking rBL[170];//rejected BookingLists
-int rBLIndex = 0;
+
+struct Booking rBL_FCFS[170];//rejected BookingLists
+int rBLIndex_FCFS = 0;
+struct Booking rBL_SJF[170];//rejected BookingLists
+int rBLIndex_SJF = 0;
+
+int Total_ReqAcc_FCFS = 0;
+int Total_ReqAcc_SJF = 0;
+int Total_ReqRej_FCFS = 0;
+int Total_ReqRej_SJF = 0;
+int AEFlag = 0;
 
 
 //pad test
@@ -41,3 +52,8 @@ int action_Create=0; // 0 = can create
 char staffNames[8][10] = {
 	"Alan","Billy","Cathy","David","Eva","Fanny","Gary","Helen"
 };
+
+int staffRr[8]={0,0,0,0,0,0,0,0};
+int staffAr_FCFS[8]={0,0,0,0,0,0,0,0};
+int staffAr_SJF[8]={0,0,0,0,0,0,0,0};
+double staffAtten[8]={0,0,0,0,0,0,0,0};
